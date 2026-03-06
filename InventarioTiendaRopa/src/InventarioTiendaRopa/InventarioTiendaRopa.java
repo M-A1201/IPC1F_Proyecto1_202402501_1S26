e6bbea7bb8a6792d0bd26ddaeab4872ed8712325
M-A1201 <2946755580408@ingenieria.usac.edu.gt>
2026-03-06 17:51:58 +0100

    primer commit del proyecto 1

---
+++ InventarioTiendaRopa/src/InventarioTiendaRopa/InventarioTiendaRopa.java
@@ -0,0 +1,15 @@
+package InventarioTiendaRopa;
+
+import InventarioTiendaRopa.views.VistaPrincipal;
+
+public class InventarioTiendaRopa {
+
+    public static void main(String[] args) {
+
+        //creamos un objeto
+        VistaPrincipal vprincipal = new VistaPrincipal();
+        vprincipal.setVisible(true);
+
+    }
+
+}
